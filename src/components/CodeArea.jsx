95587028ab8adc74616f24e9bbc4f80adb5b6aaa
DivorcedLance <divorcedlance@gmail.com>
2024-07-22 18:30:55 -0500

    chore: Update CodeArea component and styles

--- src/components/CodeArea.jsx
+++ src/components/CodeArea.jsx
@@ -1,8 +1,10 @@
 export function CodeArea() {
-  return ( 
+  return (
     <div className="w-full h-screen flex">
-      <textarea className="bg-slate-800 w-full h-full text-white text-6xl font-mono box-border px-2 pt-4 no-scrollbar resize-none">
-      </textarea>
+      <textarea
+        className="bg-slate-800 w-full h-full text-white text-6xl font-mono box-border px-2 pt-4 no-scrollbar resize-none"
+        autoFocus
+      ></textarea>
     </div>
   )
 }
